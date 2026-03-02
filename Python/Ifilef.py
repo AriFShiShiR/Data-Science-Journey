d84643c12957a9a9e35fc2f1707be225257582cf
AriFShiShiR <arifshishir98@gmail.com>
2026-03-03 00:13:18 +0600

    Ternary operator added and code  updated

--- Python/Ifilef.py
+++ Python/Ifilef.py
@@ -1,12 +1,16 @@
 balance = 100000
-
-amount =int(input("Enter withdrawal amount: "))
+amount = int(input("Enter withdrawal amount: "))
 
 if amount <= 0:
     print("Invalid amount.")
-
 elif amount > balance:
-    print ("Insaficiant balance.")
-
+    print("Insufficient balance.")
 else:
-    print("Withdrawal Sucseddfull.")
+    print("Withdrawal Successful.")
+
+
+#ternary operator
+
+value = int(input("Enter any value: "))
+result = "positive" if value > 0 else "negative" if value < 0 else "zero"
+print(result)
