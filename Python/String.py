bb08bfbfd0b328ababa895209849d8c291b774f3
AriFShiShiR <arifshishir98@gmail.com>
2026-03-04 05:27:49 +0600

    new file created

--- Python/String.py
+++ Python/String.py
@@ -8,4 +8,18 @@ print(result)
 length = len(result)
 print("Length of the string:", length)
 
-#slicing
+#String function
+
+string3 =("hi this is Arif Mahmud shishir and I am a New coder")
+str=string3
+str.replace("old","new")#Replace all ocarence of old to new.
+print(str.capitalize())#Capitalize the first Character.
+print(str.endswith("er"))#Return True if string ends with substr.
+print(str.find("New"))#Return 1st index of 1st occerance.
+print(str.count("a"))#count the occerance of substr.
+
+#Practice
+Name=input("Enter The user name: ")
+print("Lenth of user name:",len(Name))
+
+
